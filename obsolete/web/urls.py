28ab65bc17287e817a416f4981839efd54822532
Maksim Klychkov <maxim.klychkov64@gmail.com>
2024-02-16 18:49:39 +0300

    Recreate Django Project with mounted PostgreSQL. docker-compose.yml and Dockerfile added

---
+++ obsolete/web/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from core import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home),
+
+    path('sign-in/', auth_views.LoginView.as_view(template_name="sign_in.html")),
+    path('sign-out/', auth_views.LogoutView.as_view(next_page='/')),
+]
