5e0a54e7d3ef3f7f31e4ca765894a6d4e5a69360
Maksim Klychkov <maxim.klychkov64@gmail.com>
2024-03-14 22:19:46 +0300

    edited settings.py

--- web/web/settings.py
+++ web/web/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b)@rn-87y0u9m1o1pc)1l3a_=plogm(#1iol%3s3udlrsg8+1+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
